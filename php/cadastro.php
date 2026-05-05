09713ebb0a522961868fa1b784328c45d6569aba
Aluno.Etec <aula@etec.com>
2026-05-05 19:38:35 -0300

    Correção do erro da imagem-cadastro.php

--- php/cadastro.php
+++ php/cadastro.php
@@ -11,7 +11,7 @@
     <main id="cadastro" class="screen">
         <header>
             <div class="logologin">
-                <img src="images/Logo_do_TCC-P2.png" alt="Logo FutEscola">
+                <img src="/FutEscola/images/Logo_do_TCC-P2.png">
             </div>
             <h1 style="text-align: center;">Cadastre-se</h1>
         </header>
